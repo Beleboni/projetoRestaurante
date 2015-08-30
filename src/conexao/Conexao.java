3f375b5080866cf6f2209098304030e6d18d99e6
unknown <fernando.beleboni@hotmail.com>
2015-08-29 23:05:37 -0300

    Inicio do projeto

---
+++ src/conexao/Conexao.java
@@ -0,0 +1,29 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+	
+	public static Connection connection;
+	
+	static{
+		String endereco = "jdbc:mysql://localhost:3306/pequena-empresa";
+		String usuario = "root";
+		String senha = "";
+	
+	try{
+		connection = DriverManager.getConnection(endereco, usuario, senha);
+	} catch(SQLException e ){
+		System.out.println("Erro ao se conectar com o banco de dados");
+		e.printStackTrace();
+		
+		}
+	}
+	
+	public static Connection getCon(){
+		return connection;
+	}
+	
+}
