c6bf8046b757e61ea163e234fe7126330f8f0187
Fernando <fernando.beleboni@hotmail.com>
2015-11-24 20:37:05 -0200

    Novas

---
+++ src/br/com/exception/UsoException.java
@@ -0,0 +1,10 @@
+package br.com.exception;
+
+public class UsoException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public UsoException(String message){
+		super(message);
+	}
+}
